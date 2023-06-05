728211401d04e5af6bdb91b594bae2b65ed3627c
Ibrahim <109971855+ibrahim-string@users.noreply.github.com>
2023-06-05 21:40:23 +0530

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Scrapset',
-    version='0.1.1',
+    version='1.1.0',
     description='DataScraper: Effortless Dataset Extraction',
     author='Ibrahim',
     author_email='string2025@gmail.com',
